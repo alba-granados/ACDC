cba8675034a7c849b27765b5e7303ab8298267de
alba-granados <alba.granados@isardsat.cat>
2020-11-26 10:39:36 +0100

    script_bulk_processing_CCi_sea_state.m  - new paths

--- script_bulk_processing_CCi_sea_state.m
+++ script_bulk_processing_CCi_sea_state.m
@@ -9,7 +9,11 @@ mission='S3';
 
 %%  
 % input_path='/media/alba/DATA/isardSAT/coding/data/CCI_sea_state/L1A_ESA/S3A_SR_1_SRA_A__20200525T053649_20200525T062719_20200526T211407_3029_058_319______MAR_O_ST_004'  
-input_path='/media/alba/DATA/isardSAT/coding/data/CCI_sea_state/L1A_ESA/track_074/S3A_SR_1_SRA_A__20170328T105814_20170328T114844_20170423T023813_3029_016_037______MAR_O_NT_002/'
+% input_path='/media/alba/DATA/isardSAT/coding/data/CCI_sea_state/L1A_ESA/track_514/S3A_SR_1_SRA_A__20170217T211521_20170217T220550_20170315T124329_3028_014_257______MAR_O_NT_002/'
+% input_path='/media/alba/DATA/isardSAT/coding/data/CCI_sea_state/L1A_ESA/track_741/S3A_SR_1_SRA_A__20170225T201722_20170225T210751_20170323T105750_3029_014_370______MAR_O_NT_002/'
+% input_path='/media/alba/DATA/isardSAT/coding/data/CCI_sea_state/L1A_ESA/track_513/S3A_SR_1_SRA_A__20170217T202450_20170217T211519_20170315T110727_3029_014_256______MAR_O_NT_002/' 
+% input_path='/media/alba/DATA/isardSAT/coding/data/CCI_sea_state/L1A_ESA/track_513/S3A_SR_1_SRA_A__20180329T202459_20180329T211527_20180424T110744_3028_029_256______MAR_O_NT_003/' 
+input_path='/media/alba/DATA/isardSAT/coding/data/CCI_sea_state/L1A_ESA/track_513/S3A_SR_1_SRA_A__20191113T202500_20191113T211528_20191209T114804_3027_051_256______MAR_O_NT_003/' 
 
 output_path='/media/alba/DATA/isardSAT/coding/output/CCI_sea_state/ACDC/';
 dirs=split(input_path,'/');
